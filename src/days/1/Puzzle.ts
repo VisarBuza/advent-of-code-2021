cf194b7c61512b0f2b022853d48d9e6aee0c848e
Ardit Baloku <arditbaloku@gmail.com>
2021-12-02 10:19:46 +0100

    Add day 1

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -2,16 +2,31 @@ import Puzzle from '../../types/AbstractPuzzle';
 
 export default class ConcretePuzzle extends Puzzle {
   public solveFirst(): string {
-    return 'day 1 solution 1';
-  }
-  public solveSecond(): string {
-    return 'day 1 solution 2';
+    const arr = this.input.split('\n').map((x) => +x);
+    let sum = 0;
+    for (let i = 1; i < arr.length; i++) if (arr[i] > arr[i - 1]) sum++;
+
+    return sum.toString();
   }
 
   public getFirstExpectedResult(): string {
-    return 'day 1 solution 1';
+    return '7';
   }
+
+  public solveSecond(): string {
+    const arr = this.input.split('\n').map((x) => +x);
+    let sum = 0;
+    for (let i = 0; i < arr.length - 2; i++)
+      if (
+        arr[i] + arr[i + 1] + arr[i + 2] <
+        arr[i + 1] + arr[i + 2] + arr[i + 3]
+      )
+        sum++;
+
+    return sum.toString();
+  }
+
   public getSecondExpectedResult(): string {
-    return 'day 1 solution 2';
+    return '5';
   }
 }
