acc80dbf7071f609fdc91dcef29a0f5a470948e2
Ardit Baloku <arditbaloku@gmail.com>
2021-12-05 14:39:16 +0100

    Fix script

--- src/scripts/initNewDay/initNewDay.ts
+++ src/scripts/initNewDay/initNewDay.ts
@@ -9,16 +9,19 @@ import { existsSync, copyFileSync, mkdirSync, writeFileSync } from 'fs';
 
 const args = process.argv.slice(2);
 const day = args[0];
+
 if (!day) {
   console.log('Please run with the day to bootstrap, i.e. npm run init-day 1');
 }
-console.log(`creating template for day ${2}`);
+
+console.log(`creating template for day ${day}`);
 const basePath = 'src/days';
 
 if (existsSync(`src/days/${day}`)) {
   console.log(`day ${day} already exists`);
   process.exit(0);
 }
+
 const newDayPath = `${basePath}/${day}`;
 mkdirSync(newDayPath);
 copyFileSync(`${__dirname}/Puzzle.ts.tpl`, `${newDayPath}/Puzzle.ts`);
