be660ef0e0a41038e43f612bba17389ca912e8cd
Visar Buza <visar@gjirafa.com>
2021-12-06 12:04:01 +0100

    Modify day 3 solution

--- src/days/3/Puzzle.ts
+++ src/days/3/Puzzle.ts
@@ -3,10 +3,11 @@ import Puzzle from '../../types/AbstractPuzzle';
 export default class ConcretePuzzle extends Puzzle {
   public solveFirst(): string {
     const arr = this.input.split('\n');
-    const bitFrequencies = this.getBitFrequencies(arr);
+    const bitLength = arr[0].length;
+    const bitFrequencies = this.getBitFrequencies(arr, bitLength);
 
-    const gammaRate = bitFrequencies.map(x => x >= 500 ? '1' : '0').join('');
-    const epsilonRate = bitFrequencies.map(x => x <= 500 ? '1' : '0').join('');
+    const gammaRate = bitFrequencies.map(x => x >= arr.length / 2 ? '1' : '0').join('');
+    const epsilonRate = bitFrequencies.map(x => x <= arr.length / 2 ? '1' : '0').join('');
 
     return (parseInt(gammaRate, 2) * parseInt(epsilonRate, 2)).toString();
   }
@@ -17,13 +18,14 @@ export default class ConcretePuzzle extends Puzzle {
 
   public solveSecond(): string {
     const arr = this.input.split('\n');
+    const bitLength = arr[0].length;
 
     let oxygenGeneratorRating = arr;
     let co2Rating = arr;
 
     let index = 0;
     while (oxygenGeneratorRating.length > 1) {
-      const frequency = this.getBitFrequencies(oxygenGeneratorRating);
+      const frequency = this.getBitFrequencies(oxygenGeneratorRating, bitLength);
       const majority = frequency[index] >= Math.ceil(oxygenGeneratorRating.length / 2) ? '1' : '0';
       oxygenGeneratorRating = oxygenGeneratorRating.filter(x => x[index] == majority);
       index++;
@@ -32,7 +34,7 @@ export default class ConcretePuzzle extends Puzzle {
     index = 0;
 
     while (co2Rating.length > 1) {
-      const frequency = this.getBitFrequencies(co2Rating);
+      const frequency = this.getBitFrequencies(co2Rating, bitLength);
       const majority = frequency[index] < Math.ceil(co2Rating.length / 2) ? '1' : '0';
       co2Rating = co2Rating.filter(x => x[index] == majority);
       index++;
@@ -41,8 +43,8 @@ export default class ConcretePuzzle extends Puzzle {
     return (parseInt(oxygenGeneratorRating[0], 2) * parseInt(co2Rating[0], 2)).toString();
   }
 
-  public getBitFrequencies(input: string[]) {
-    const bitFrequencies = new Array(12).fill(0);
+  public getBitFrequencies(input: string[], bitLength: number) {
+    const bitFrequencies = new Array(bitLength).fill(0);
 
     for (let i = 0; i < input.length; i++) {
       for (let j = 0; j < input[i].length; j++) {
